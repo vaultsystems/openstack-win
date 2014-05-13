0be47aa591c4e3280f9ae19aaf07fba1769510ca
noma4i <noma4i@gmail.com>
2014-05-13 14:58:48 +1000

    we prefer vnc

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ fi
 
 qemu-img create -f qcow2 -o preallocation=metadata $IMAGE 16G
 
-$KVM -m 2048 -smp 2 -cdrom $ISO -drive file=$VIRTIO_ISO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us
+$KVM -m 2048 -smp 2 -cdrom $ISO -drive file=$VIRTIO_ISO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
