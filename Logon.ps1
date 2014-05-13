fe66323df1639337735b50d72032e08a7a30c4ac
noma4i <noma4i@gmail.com>
2014-05-13 18:27:24 +1000

    workaround finale

--- Logon.ps1
+++ Logon.ps1
@@ -33,10 +33,7 @@ try
     $masterServer = "puppet"
 
     Invoke-WebRequest $puppetUrl -OutFile $puppetFile
-    Start-Process -FilePath msiexec -ArgumentList /i, $puppetFile, /qn "PUPPET_MASTER_SERVER=$masterServer"
-
-
-    & "$ENV:SystemRoot\System32\msiexec.exe.exe /qn /i '$puppetFile' PUPPET_MASTER_SERVER=$masterServer"
+    Start-Process -FilePath msiexec -ArgumentList /i, "$puppetFile PUPPET_MASTER_SERVER=$masterServer", /qn
 
     # Finalize and cleanup
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
