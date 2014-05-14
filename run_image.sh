8c0e5a66a1c1e415a142a0f081ba10e3f10a34c5
noma4i <noma4i@gmail.com>
2014-05-14 11:38:11 +1000

    run resulting image

---
+++ run_image.sh
@@ -0,0 +1,8 @@
+IMAGE=windows-server-2012-r2.qcow2
+
+KVM=/usr/libexec/qemu-kvm
+if [ ! -f "$KVM" ]; then
+    KVM=/usr/bin/kvm
+fi
+
+$KVM -m 2048 -smp 2 -drive file=$IMAGE -boot d -vga std -k en-us -vnc :1
