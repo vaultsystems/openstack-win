4363314269bfda380649f9b490665f6f5b483140
Christoph Dwertmann <cdwertmann@gmail.com>
2014-05-28 00:37:27 -0400

    Testing with updates enabled

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 IMAGE=windows-server-2012-r2.raw
 FLOPPY=Autounattend.vfd
 VIRTIO_ISO=virtio-win-0.1-74.iso
-ISO=9600.16384.WINBLUE_RTM.130821-1623_X64FRE_SERVER_EVAL_EN-US-IRM_SSS_X64FREE_EN-US_DV5.ISO
+ISO=$1
 
 KVM=/usr/libexec/qemu-kvm
 if [ ! -f "$KVM" ]; then
     KVM=/usr/bin/kvm
 fi
 
+if [ "$#" -ne 1 ]; then
+    echo "Missing ISO file"
+fi
+
 #qemu-img create -f qcow2 -o preallocation=metadata $IMAGE 16G
 truncate --size=0 $IMAGE
 truncate --size=16G $IMAGE
 
-$KVM -m 2048 -smp 2 -cdrom $ISO -drive file=$VIRTIO_ISO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
+$KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 2048 -smp 2 -cdrom $ISO -drive file=$VIRTIO_ISO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
