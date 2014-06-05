4fcb3dab9fd42f0fff16ec9fe0a6fed327455a10
noma4i <noma4i@gmail.com>
2014-06-05 16:02:34 +1000

    writing to log

--- meta-data.ps1
+++ meta-data.ps1
@@ -1,7 +1,16 @@
-$dataUrl = "http://169.254.169.254/latest/meta-data"
-$hostName = Invoke-WebRequest "$dataUrl/local-hostname" | foreach {$_.Content}
+try
+{
+  $dataUrl = "http://169.254.169.254/latest/meta-data"
+  $hostName = Invoke-WebRequest "$dataUrl/local-hostname" | foreach {$_.Content}
 
-if (${env:computerName} -ne $hostName){
-  Rename-Computer $hostName
-  Restart-Computer -Force
+  if (${env:computerName} -ne $hostName){
+    Rename-Computer $hostName
+    Restart-Computer -Force
+  }
+}
+catch
+{
+    $log = Get-EventLog -List | Where-Object { $_.Log -eq "Application" }
+    $log.Source = "OpenStack"
+    $log.WriteEntry("TRAPPED: $_.Exception.ToString()", [system.Diagnostics.EventLogEntryType]::Error,1234)
 }
