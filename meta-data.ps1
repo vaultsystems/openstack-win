625133b9137ebe9e4f550b0c00ee74687f3611e7
noma4i <noma4i@gmail.com>
2014-06-06 11:41:12 +1000

    call script to setup host name in specialize section

--- meta-data.ps1
+++ meta-data.ps1
@@ -1,5 +1,6 @@
 try
 {
+   $Host.UI.RawUI.WindowTitle = "Setup Host"
   $dataUrl = "http://169.254.169.254/latest/meta-data"
   $hostName = Invoke-WebRequest "$dataUrl/local-hostname" | foreach {$_.Content}
 
