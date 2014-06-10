32c58f8bf1ec123beddfcb9ab78a078f600281e8
noma4i <noma4i@gmail.com>
2014-06-10 14:57:26 +1000

    testing is over

--- meta-data.ps1
+++ meta-data.ps1
@@ -1,6 +1,6 @@
 $Host.UI.RawUI.WindowTitle = "Setup Host"
 $dataUrl = "http://169.254.169.254/latest/meta-data"
-$hostName = Invoke-WebRequest "https://gist.githubusercontent.com/noma4i/573198ec8246607aa10a/raw/1da130cec1aa0718cf442d02b6a66a880aa7c072/gistfile1.txt" | foreach {$_.Content.split(".")[0].substring(0,10).toUpper()}
+$hostName = Invoke-WebRequest "$dataUrl/local-hostname" | foreach {$_.Content.split(".")[0].substring(0,10).toUpper()}
 
 if ((${env:computerName} -ne $hostName) -and ($hostName -ne $null)){
   Rename-Computer $hostName
