ff083f50a02d173a86f3fe2c76146f6ff3b665e7
noma4i <noma4i@gmail.com>
2014-06-12 12:04:04 +1000

    workaround

--- meta-data.ps1
+++ meta-data.ps1
@@ -1,7 +1,11 @@
 $Host.UI.RawUI.WindowTitle = "Setup Host"
 $dataUrl = "http://169.254.169.254/latest/meta-data"
-$hostName = Invoke-WebRequest "https://gist.githubusercontent.com/noma4i/573198ec8246607aa10a/raw/1da130cec1aa0718cf442d02b6a66a880aa7c072/gistfile1.txt" | foreach {$_.Content.split(".")[0].substring(0,10).toUpper()}
+# $hostName = Invoke-WebRequest "https://gist.githubusercontent.com/noma4i/573198ec8246607aa10a/raw/1da130cec1aa0718cf442d02b6a66a880aa7c072/gistfile1.txt" | foreach {$_.Content.split(".")[0].substring(0,10).toUpper()}
+Start-Sleep -s 60
+Invoke-WebRequest "$dataUrl" -OutFile 'c:\meta-data.txt'
+Invoke-WebRequest "$dataUrl/local-hostname" -OutFile 'c:\local-hostname.txt'
 
+$hostName = Invoke-WebRequest "$dataUrl/local-hostname" | foreach {$_.Content.split(".")[0].substring(0,14).toUpper()}
 if ((${env:computerName} -ne $hostName) -and ($hostName -ne $null)){
   Rename-Computer $hostName
 
