dc14a387d12b8548153e3a8b0264f8371c39c36a
Christoph Dwertmann <cdwertmann@gmail.com>
2014-06-23 21:00:51 -0400

    disk is 20GB

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ if [ "$#" -ne 1 ]; then
     echo "Missing ISO file"
 fi
 
-#qemu-img create -f qcow2 -o preallocation=metadata $IMAGE 16G
+#qemu-img create -f qcow2 -o preallocation=metadata $IMAGE 20G
 truncate --size=0 $IMAGE
-truncate --size=16G $IMAGE
+truncate --size=20G $IMAGE
 
 $KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 2048 -smp 2 -cdrom $ISO -drive file=$VIRTIO_ISO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
