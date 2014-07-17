d07c18e5acbbea25e3b78a525f3d30185952d699
noma4i <noma4i@gmail.com>
2014-07-17 14:41:44 +1000

    Workaround for fetch name process

--- meta-data.ps1
+++ meta-data.ps1
@@ -1,21 +1,30 @@
-$Host.UI.RawUI.WindowTitle = "Setup Host"
-$dataUrl = "http://169.254.169.254/latest/meta-data"
-$hostNameRaw = Invoke-WebRequest "$dataUrl/local-hostname" | foreach {$_.Content.split(".")[0]}
+function fetch_data(){
+  try{
+    $Host.UI.RawUI.WindowTitle = "Setup Host"
+    $dataUrl = "http://169.254.169.254/latest/meta-data"
+    $hostNameRaw = Invoke-WebRequest "$dataUrl/local-hostname" | foreach {$_.Content.split(".")[0]}
 
-$hostName = $hostNameRaw.toUpper()
-$finishFlag = "$ENV:SystemRoot\System32\finish.flg"
-if ((${env:computerName} -ne $hostName) -and ($hostName -ne $null)){
-  if ((Test-Path $finishFlag) -ne $true){
-    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
-    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name AutoAdminLogon -value 0
+    $hostName = $hostNameRaw.toUpper()
+    $finishFlag = "$ENV:SystemRoot\System32\finish.flg"
+    if ((${env:computerName} -ne $hostName) -and ($hostName -ne $null)){
+      if ((Test-Path $finishFlag) -ne $true){
+        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
+        Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name AutoAdminLogon -value 0
 
-    # Expire Administrator password
-    $user = [ADSI]'WinNT://localhost/Administrator'
-    $user.passwordExpired = 1
-    $user.setinfo()
+        # Expire Administrator password
+        $user = [ADSI]'WinNT://localhost/Administrator'
+        $user.passwordExpired = 1
+        $user.setinfo()
 
-    Set-Content -Path "$finishFlag" -Value "done"
-  }
-  Set-Service -name puppet -startupType Automatic
-  Rename-Computer $hostName -Force -Restart
+        Set-Content -Path "$finishFlag" -Value "done"
+      }
+      Set-Service -name puppet -startupType Automatic
+      Rename-Computer $hostName -Force -Restart
+    }
+    } catch {
+      Start-Sleep -s 30
+      fetch_data
+    }
 }
+
+fetch_data
