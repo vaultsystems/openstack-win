157ff6ef74722ec03e0535c4e52b7bcad4fa5d02
Christoph Dwertmann <cdwertmann@gmail.com>
2015-01-13 18:10:13 +1100

    reduce to 25GB image size

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ fi
 
 #qemu-img create -f qcow2 -o preallocation=metadata $IMAGE 20G
 truncate --size=0 $IMAGE
-truncate --size=30G $IMAGE
+truncate --size=25G $IMAGE
 
 $KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 4096 -smp 4 -cdrom $ISO -drive file=$VIRTIO.iso,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
