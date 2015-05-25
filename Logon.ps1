58278b4d518f100f353c88b20ed28a50e8fbee77
Gobin Sougrakpam <gobin.sougrakpam@vaultsystems.com.au>
2015-05-25 12:06:10 +1000

    Commenting Update steps for testing

--- Logon.ps1
+++ Logon.ps1
@@ -26,14 +26,14 @@ try
       $psWindowsUpdateUrl = "https://raw.githubusercontent.com/vaultsystems/openstack-win/master/PSWindowsUpdate.zip"
       $psWindowsUpdateFile = "$admFolder\PSWindowsUpdate.zip"
 
-      Invoke-WebRequest $psWindowsUpdateUrl -OutFile $psWindowsUpdateFile
-      foreach($item in (New-Object -com shell.application).NameSpace($psWindowsUpdateFile).Items())
-      {
-        $yesToAll = 16
-        (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
-      }
-      Import-Module PSWindowsUpdate
-      Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
+      #Invoke-WebRequest $psWindowsUpdateUrl -OutFile $psWindowsUpdateFile
+      #foreach($item in (New-Object -com shell.application).NameSpace($psWindowsUpdateFile).Items())
+      #{
+      #  $yesToAll = 16
+      #  (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
+      #}
+      #Import-Module PSWindowsUpdate
+      #Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
 
       #SetComputername
       Rename-Computer "dummy"
