9d1667ee0b3f48ae766e660852b73e8fada56452
Christoph Dwertmann <cdwertmann@gmail.com>
2015-07-30 12:18:01 +1000

    missing quote

--- jenkins_rebuild_eval.sh
+++ jenkins_rebuild_eval.sh
@@ -5,7 +5,7 @@ export ISO=/home/cdw/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_E
 time ./build.sh $ISO
 #glance index | grep "$GLANCE_NAME" | awk '{ print $1}'| xargs --no-run-if-empty -L1 glance image-delete
 glance image-create --name "$GLANCE_NAME" --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
-TROOP_IMAGE=`glance image-list | grep "Windows 2012 R2 autobuild" | sort -u -k8 | tail -1 | awk '{ print $2}'
+TROOP_IMAGE=`glance image-list | grep "Windows 2012 R2 autobuild" | sort -u -k8 | tail -1 | awk '{ print $2}'`
 ID=`cinder create --image-id=$TROOP_IMAGE 25 | grep " id " | awk '{ print $4}'`
 sleep 3600
 cinder show $ID
