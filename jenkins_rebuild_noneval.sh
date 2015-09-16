a00b33544ff6159caff7369d336c320f9a646184
Christoph Dwertmann <cdwertmann@gmail.com>
2015-09-17 00:17:42 +1000

    jenkins build number

--- jenkins_rebuild_noneval.sh
+++ jenkins_rebuild_noneval.sh
@@ -4,4 +4,4 @@ export ISO=/home/cdw/SW_DVD9_Windows_Svr_Std_and_DataCtr_2012_R2_64Bit_English_-
 ./create-autounattend-floppy.sh
 time ./build.sh $ISO
 glance image-list | grep "$GLANCE_NAME" | awk '{ print $2}'| xargs --no-run-if-empty -L1 glance image-delete
-glance image-create --name "$GLANCE_NAME" --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
+glance image-create --name "$GLANCE_NAME" --property jenkins_build=$BUILD_NUMBER --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
