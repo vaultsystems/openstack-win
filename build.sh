e6f6def48b6dbafac8fe49a7b9d279fb0e55c892
Alexander Tsirel <noma4i@gmail.com>
2015-10-06 13:52:13 +1100

    Revert "Possible solution for black screen in qemu"
    This reverts commit 0df82389848931dc2c09d78bc4baa0d12499690d.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 IMAGE=windows-server-2012-r2.raw
 FLOPPY=Autounattend.vfd
-VIRTIO=virtio-win-0.1-100.iso
+VIRTIO=virtio-win-0.1-81.iso
 ISO=$1
 
 wget --quiet -nc http://ftp.sleepgate.ru/drivers/NET/virtio-win/$VIRTIO
@@ -18,4 +18,4 @@ fi
 truncate --size=0 $IMAGE
 truncate --size=25G $IMAGE
 
-$KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 4096 -smp 4 -cdrom $ISO -drive file=$VIRTIO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
+$KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 4096 -smp 4 -cdrom $ISO -drive file=$VIRTIO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :2
