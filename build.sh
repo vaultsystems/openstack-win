7aba331c05246e3af18ab311dbb5fa71ba96c758
Christoph Dwertmann <cdwertmann@gmail.com>
2015-10-08 11:54:10 +1100

    use official virtio mirror

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 IMAGE=windows-server-2012-r2.raw
 FLOPPY=Autounattend.vfd
-VIRTIO=virtio-win-0.1-100.iso
+VIRTIO=0.1.102
 ISO=$1
 
-wget --quiet -nc http://ftp.sleepgate.ru/drivers/NET/virtio-win/$VIRTIO
+wget --quiet -nc https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-$VIRTIO/virtio-win-$VIRTIO.iso -O virtio-win.iso
 
 KVM=/usr/libexec/qemu-kvm
 if [ ! -f "$KVM" ]; then
@@ -18,4 +18,4 @@ fi
 truncate --size=0 $IMAGE
 truncate --size=25G $IMAGE
 
-$KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 4096 -smp 4 -cdrom $ISO -drive file=$VIRTIO,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
+$KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 4096 -smp 4 -cdrom $ISO -drive file=virtio-win.iso,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
