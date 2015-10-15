24004c766a15346fed10f791da1d38a3ead3a87c
Christoph Dwertmann <cdwertmann@gmail.com>
2015-10-15 22:15:15 +1100

    use latest virtio 110

---
+++ convert_virtio_iso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# the folder layout of the virtio changed recently
+# use this script to convert it to the old layout
+
+# usage: ./convert_virtio_iso.sh virtio-win.iso virtio-converted.iso
+
+ORIG=/tmp/original_virtio
+NEW=/tmp/new_virtio
+mkdir -p $NEW/WIN8/AMD64 $ORIG
+7z x $1 -o$ORIG
+find $ORIG -type f -wholename '*2k12/amd64*' -exec mv {} $NEW/WIN8/AMD64/ \;
+mkisofs -o $2 $NEW
+rm -rf $NEW $ORIG
