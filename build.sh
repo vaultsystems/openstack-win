273a8f881150fe5f6d58548ff92af0fe95ab3555
Christoph Dwertmann <cdwertmann@gmail.com>
2015-10-22 23:42:29 +1100

    new autounattend ISO builder

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ VIRTIO=virtio-win-0.1.110-2
 ISO=$1
 wget -N https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/$VIRTIO/virtio-win.iso
 ./convert_virtio_iso.sh virtio-win.iso virtio-converted.iso
+mkisofs -J -r -o autounattend.iso config
 
 KVM=/usr/libexec/qemu-kvm
 if [ ! -f "$KVM" ]; then
@@ -18,4 +19,4 @@ fi
 truncate --size=0 $IMAGE
 truncate --size=25G $IMAGE
 
-$KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 4096 -smp 4 -cdrom $ISO -drive file=virtio-converted.iso,index=3,media=cdrom -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :1
+$KVM -machine accel=kvm:tcg -machine pc-i440fx-1.5,accel=kvm,usb=off -cpu SandyBridge,+erms,+smep,+fsgsbase,+pdpe1gb,+rdrand,+f16c,+osxsave,+dca,+pcid,+pdcm,+xtpr,+tm2,+est,+smx,+vmx,+ds_cpl,+monitor,+dtes64,+pbe,+tm,+ht,+ss,+acpi,+ds,+vme -m 4096 -smp 4 -cdrom $ISO -drive file=virtio-converted.iso,index=3,media=cdrom -cdrom autounattend.iso $IMAGE -boot d -vga std -k en-us -vnc :1
--- jenkins_rebuild_noneval.sh
+++ jenkins_rebuild_noneval.sh
@@ -1,7 +1,6 @@
 set -x
 export GLANCE_NAME="Windows 2012 R2 NON-EVAL"
 export ISO=/home/cdw/SW_DVD9_Windows_Svr_Std_and_DataCtr_2012_R2_64Bit_English_-3_MLF_X19-53588.ISO
-./create-autounattend-floppy.sh
 time ./build.sh $ISO
 glance image-list | grep "$GLANCE_NAME" | awk '{ print $1}'| xargs --no-run-if-empty -L1 glance image-delete
 glance image-create --name "$GLANCE_NAME" --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
