ddb18bbb1c51f6046b23c16857606875133bbaec
Christoph Dwertmann <cdwertmann@gmail.com>
2015-10-23 12:51:39 +1100

    fix properties

--- jenkins_rebuild_eval.sh
+++ jenkins_rebuild_eval.sh
@@ -3,4 +3,4 @@ export GLANCE_NAME="Windows 2012 R2 autobuild $BUILD_NUMBER"
 export ISO=/home/cdw/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO
 time ./build.sh $ISO
 #glance index | grep "$GLANCE_NAME" | awk '{ print $1}'| xargs --no-run-if-empty -L1 glance image-delete
-glance image-create --name "$GLANCE_NAME" --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
+glance image-create --name "$GLANCE_NAME" --property jenkins_build $BUILD_NUMBER --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
--- jenkins_rebuild_noneval.sh
+++ jenkins_rebuild_noneval.sh
@@ -2,5 +2,5 @@ set -x
 export GLANCE_NAME="Windows 2012 R2 NON-EVAL"
 export ISO=/home/cdw/SW_DVD9_Windows_Svr_Std_and_DataCtr_2012_R2_64Bit_English_-3_MLF_X19-53588.ISO
 time ./build.sh $ISO
-glance image-list | grep "$GLANCE_NAME" | awk '{ print $1}'| xargs --no-run-if-empty -L1 glance image-delete
-glance image-create --name "$GLANCE_NAME" --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
+glance image-list | grep "$GLANCE_NAME" | awk '{ print $2}'| xargs --no-run-if-empty -L1 glance image-delete
+glance image-create --name "$GLANCE_NAME" --property jenkins_build $BUILD_NUMBER --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
