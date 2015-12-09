07ce914113f4571ffa779a6e64e489c27d5857e8
Christoph Dwertmann <cdwertmann@gmail.com>
2015-12-10 09:10:58 +1100

    disable updates

--- Logon.ps1
+++ Logon.ps1
@@ -33,7 +33,7 @@ try
         (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
       }
       Import-Module PSWindowsUpdate
-      Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
+      #Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
 
       # Set Computername
       Rename-Computer "dummy"
