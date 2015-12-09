a9a6a7f8f6711593b4ec116d088e50b128d52800
Christoph Dwertmann <cdwertmann@gmail.com>
2015-12-09 15:16:02 +1100

    re-enable cloud init

--- Logon.ps1
+++ Logon.ps1
@@ -40,44 +40,44 @@ try
       Restart-Computer -Force
 
   } else {
-      # Install Software
-      #Setup RAM
-      $imDiskUrl = "https://raw.githubusercontent.com/vaultsystems/openstack-win/master/imdisk.zip"
-      $imDiskFile = "$admFolder\imdisk.zip"
+      # # Install Software
+      # # Setup RAMdisk
+      # $imDiskUrl = "https://raw.githubusercontent.com/vaultsystems/openstack-win/master/imdisk.zip"
+      # $imDiskFile = "$admFolder\imdisk.zip"
 
-      Invoke-WebRequest $imDiskUrl -OutFile $imDiskFile
-      foreach($item in (New-Object -com shell.application).NameSpace($imDiskFile).Items())
-      {
-        $yesToAll = 16
-        (New-Object -com shell.application).NameSpace("C:\").copyhere($item, $yesToAll)
-      }
+      # Invoke-WebRequest $imDiskUrl -OutFile $imDiskFile
+      # foreach($item in (New-Object -com shell.application).NameSpace($imDiskFile).Items())
+      # {
+      #   $yesToAll = 16
+      #   (New-Object -com shell.application).NameSpace("C:\").copyhere($item, $yesToAll)
+      # }
 
-      & rundll32 setupapi.dll,InstallHinfSection DefaultInstall 128 C:\imdisk\imdisk.inf
+      # & rundll32 setupapi.dll,InstallHinfSection DefaultInstall 128 C:\imdisk\imdisk.inf
 
-      #Setup Python
-      $pythonUrl = "https://www.python.org/ftp/python/2.7.10/python-2.7.10.amd64.msi"
-      $pythonFile = "$admFolder\python2.7.msi"
-      $pipUrl = "https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py"
-      $pipFile = "$admFolder\pip.py"
+      # #Setup Python
+      # $pythonUrl = "https://www.python.org/ftp/python/2.7.10/python-2.7.10.amd64.msi"
+      # $pythonFile = "$admFolder\python2.7.msi"
+      # $pipUrl = "https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py"
+      # $pipFile = "$admFolder\pip.py"
 
-      Invoke-WebRequest $pythonUrl -OutFile $pythonFile
-      Invoke-WebRequest $pipUrl -OutFile $pipFile
+      # Invoke-WebRequest $pythonUrl -OutFile $pythonFile
+      # Invoke-WebRequest $pipUrl -OutFile $pipFile
 
-      Start-Process "$pythonFile" /qn -Wait
-      Start-Sleep -s 20 #ensure it was done
+      # Start-Process "$pythonFile" /qn -Wait
+      # Start-Sleep -s 20 #ensure it was done
 
-      [Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Python27\;C:\Python27\Scripts\", "Machine")
-      [Environment]::SetEnvironmentVariable("PATHEXT", "$env:PATHEXT;.PY", "Machine")
+      # [Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Python27\;C:\Python27\Scripts\", "Machine")
+      # [Environment]::SetEnvironmentVariable("PATHEXT", "$env:PATHEXT;.PY", "Machine")
 
-      $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-      $env:PATHEXT = [System.Environment]::GetEnvironmentVariable("PATHEXT","Machine")
+      # $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+      # $env:PATHEXT = [System.Environment]::GetEnvironmentVariable("PATHEXT","Machine")
 
-      # Install PIP
-      & python $pipFile
-      # netifaces wants to have MSVC installed when using easy_install. Using pip as workaround.
-      & pip install netifaces
-      & easy_install -Z six python-keystoneclient python-swiftclient
-      Copy-Item C:\Python27\Scripts\swift-script.py C:\Python27\Scripts\swift.py
+      # # Install PIP
+      # & python $pipFile
+      # # netifaces wants to have MSVC installed when using easy_install. Using pip as workaround.
+      # & pip install netifaces
+      # & easy_install -Z six python-keystoneclient python-swiftclient
+      # Copy-Item C:\Python27\Scripts\swift-script.py C:\Python27\Scripts\swift.py
 
       # # Downloading PuppetAgent and pointing to server
       # $puppetUrl = "http://downloads.puppetlabs.com/windows/puppet-3.6.2.msi"
@@ -89,14 +89,14 @@ try
       # Start-Process -FilePath msiexec -ArgumentList /i, "$puppetFile PUPPET_MASTER_SERVER=$masterServer", /qn
       # Start-Sleep -s 60 #ensure it was done
 
-      # # Download and Install Cloud-Init
-      # $cloudinitUrl="https://www.cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi"
-      # $cloudinitInstaller = "$admFolder\CloudbaseInitSetup.msi"
-      # Invoke-WebRequest $cloudinitUrl -OutFile $cloudinitInstaller
-      # Start-Process -FilePath msiexec -ArgumentList " /i $admFolder\CloudbaseInitSetup.msi /qn /l*v $admFolder\Cloudinit_Install.log" -Wait
-      # sc config cloudbase-init start=disabled
-      # (Get-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf") -replace('^username=Admin$','username=Administrator') | Set-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf"
-      # (Get-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init-unattend.conf") -replace('^username=Admin$','username=Administrator') | Set-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init-unattend.conf"
+      # Download and Install Cloud-Init
+      $cloudinitUrl="https://www.cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi"
+      $cloudinitInstaller = "$admFolder\CloudbaseInitSetup.msi"
+      Invoke-WebRequest $cloudinitUrl -OutFile $cloudinitInstaller
+      Start-Process -FilePath msiexec -ArgumentList " /i $admFolder\CloudbaseInitSetup.msi /qn /l*v $admFolder\Cloudinit_Install.log" -Wait
+      sc config cloudbase-init start=disabled
+      (Get-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf") -replace('^username=Admin$','username=Administrator') | Set-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf"
+      (Get-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init-unattend.conf") -replace('^username=Admin$','username=Administrator') | Set-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init-unattend.conf"
 
       # # Install Git
       # $gitUrl="https://github.com/git-for-windows/git/releases/download/v2.6.3.windows.1/Git-2.6.3-64-bit.exe"
