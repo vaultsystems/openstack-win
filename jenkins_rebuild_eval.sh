b9a65e8fe463814c97cfe6b7b2c84184ec610ab8
Christoph Dwertmann <cdwertmann@gmail.com>
2016-08-23 13:04:50 +1000

    add ceph scsi properties

--- jenkins_rebuild_eval.sh
+++ jenkins_rebuild_eval.sh
@@ -2,4 +2,4 @@ set -x
 export GLANCE_NAME="Windows 2012 R2 autobuild $BUILD_NUMBER"
 export ISO=/home/cdw/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO
 time ./build.sh $ISO 1
-glance image-create --name "$GLANCE_NAME" --property hw_cpu_sockets=1 --property jenkins_build=$BUILD_NUMBER --property description=$ISO --property os_type=windows --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
+glance image-create --name "$GLANCE_NAME" --property hw_cpu_sockets=1 --property jenkins_build=$BUILD_NUMBER --property description=$ISO --property os_type=windows --property hw_scsi_model=virtio-scsi --property hw_disk_bus=scsi --property hw_qemu_guest_agent=yes --property os_require_quiesce=yes --disk-format raw --container-format bare --is-public true < windows-server-2012-r2.raw
